07f4d32a9855b49902d63a7e88766a7ecfc540c2
taehoon-noh <hollyth2v@gmail.com>
2018-12-28 21:07:08 +0900

    Initial commit

---
+++ src/main/java/korkmk/front/yeson/YesonApplication.java
@@ -0,0 +1,14 @@
+package korkmk.front.yeson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesonApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/korkmk/front/yeson/web/WebRestController.java
@@ -0,0 +1,13 @@
+package korkmk.front.yeson.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
