c37a5528ca936aaf05c994134d6f308f79c0f3d2
taehoon-noh <hollyth2v@gmail.com>
2018-12-28 22:45:48 +0900

    second

--- src/main/java/korkmk/front/yeson/YesonApplication.java
+++ src/main/java/korkmk/front/yeson/YesonApplication.java
@@ -2,7 +2,9 @@ package korkmk.front.yeson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class YesonApplication {
 
--- src/main/java/korkmk/front/yeson/web/WebRestController.java
+++ src/main/java/korkmk/front/yeson/web/WebRestController.java
@@ -1,13 +1,26 @@
 package korkmk.front.yeson.web;
 
+import korkmk.front.yeson.domain.posts.PostsRepository;
+import korkmk.front.yeson.domain.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
